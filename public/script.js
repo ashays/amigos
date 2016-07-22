619b806389c03e6cbce8511ef6e14684365eecfa
nbawa95 <nbawa95@gmail.com>
2016-07-22 03:05:10 +0200

    meet() kinda works

--- public/script.js
+++ public/script.js
@@ -23,3 +23,79 @@ $(document).ready(function (){
 	});
 
 });
+
+function getMahLocation() {
+	var locs = [];
+	var startPos;
+  	var geoSuccess = function(position) {
+	    startPos = position;
+	    var user = firebase.auth().currentUser;
+	    locs[0] = startPos.coords.latitude;
+	    locs[1] = startPos.coords.longitude;
+	    console.log(startPos.coords.latitude);
+  		console.log(startPos.coords.longitude);
+  		firebase.database().ref(user.uid).set({
+		    status: "active",
+		    latitude: locs[0],
+		  	longitude: locs[1],
+		  	status: "active",
+		  	matched: null
+		});
+  	};
+  	navigator.geolocation.getCurrentPosition(geoSuccess);
+
+  	return locs;
+}
+
+function getDistance(lat1, lon1, lat2, lon2) {
+	var R = 6371; // Radius of the earth in km
+	var dLat = deg2rad(lat2-lat1);  // deg2rad below
+	var dLon = deg2rad(lon2-lon1); 
+	var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2); 
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+	var d = R * c; // Distance in km
+	return d;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
+}
+
+function meet() {
+	var user = firebase.auth().currentUser;
+	var locs = getMahLocation();
+	var topMatch = null;
+	//console.log(locs[1],locs[0]);
+
+	/*query.once("value")
+  	.then(function(snapshot) {
+    snapshot.forEach(function(childSnapshot) {
+      // key will be "ada" the first time and "alan" the second time
+      var matchLat = childSnapshot.key.child("Latitude");
+      var matchLong = childSnapshot.key.child("Longitude");
+
+      if (matchLat.exists() && matchLong.exists() && getDistance(matchLat, matchLong, locs[0], locs[1]) < .5 
+      	&& userID != childSnapshot.key
+      	 && childSnapshot.key.child("Status").compareToIgnoreCase("active")) {
+      		//COMPARE FOR SIMALARITY KEEP TRACK OF MOST SIMILAR
+      		console.log("hello");
+    }
+      // childData will be the actual contents of the child
+      //UPDATE USERID AND MATCH ID WITH EACH OTHERS ID
+  });
+});*/
+  	/*if (topMatch != null) {
+  		ref.child(userID).set({
+  			Matched: topMatch,
+  			status: "passive"
+  		})
+  		ref.child(topMatch).set({
+  			Matched: userID,
+  			status: "passive"
+  		});
+
+  	}*/
+}
